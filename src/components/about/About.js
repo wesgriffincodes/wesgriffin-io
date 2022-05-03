73a6811d5b622c6f1b936546dff0459b46612cd1
Wes Griffin <wesgriffincodes@gmail.com>
2022-05-03 12:45:59 -0700

    small

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -84,7 +84,7 @@ const About = () => {
             </div>
             <div>
               <h3>Alchemy Code Lab</h3>
-              <p>- 2019</p>
+              <p>- 209</p>
             </div>
           </div>
         </div>
